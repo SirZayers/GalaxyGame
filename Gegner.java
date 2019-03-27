7338b9c45edd2905543b2112370410077bd5e956
falkenauge05 <48785319+falkenauge05@users.noreply.github.com>
2019-03-27 08:23:26 +0100

    Update Gegner.java

--- Gegner.java
+++ Gegner.java
@@ -1,4 +1,8 @@
 
 public interface Gegner {
-
+  //legt die Geschwindigkeit des Gegners fest
+  void setSpeed(int speed);
+  //legt die Geschossrate des Gegners fest
+  void setBulletFrequency(int frequency);
+    
 }
