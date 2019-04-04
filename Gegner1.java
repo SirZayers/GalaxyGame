b3a7e9325758dce223b6f87c58783bccd73c26d4
falkenauge05 <48785319+falkenauge05@users.noreply.github.com>
2019-04-04 08:42:27 +0200

    Update Gegner1.java

--- Gegner1.java
+++ Gegner1.java
@@ -1,12 +1,28 @@
 public class Gegner1 implements Gegner{
-	private int speed, bulletFrequency, xValue, yValue, length;
-	public Gegner1(int s, int b, int x, int y, int l) {
-		speed=s;
-		bulletFrequency=b;
-		xValue=x;
-		yValue=y;
-		length=l;
-	}
-	
-	
+    private int speed, bulletFrequency, xValue, yValue, length;
+    public Gegner1(int s, int b, int x, int y, int l) {
+        speed=s;
+        bulletFrequency=b;
+        xValue=x;
+        yValue=y;
+        length=l;
+    }
+    
+    public void setSpeed(int s){
+       speed=s;
+    }
+    
+    public int getSpeed(){
+        return speed;
+    }
+    
+    public void setBulletFrequency(int b){
+        bulletFrequency=b;
+    }
+    
+    public int getBulletFrequency(){
+        return bulletFrequency;
+    }
+    
+    
 }
