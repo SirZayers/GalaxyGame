0523f1f5a881964a774711b6cebba5a6d026c2ee
Senfmeier <48903950+Senfmeier@users.noreply.github.com>
2019-04-04 08:04:25 +0200

    Update Spieler.java

--- Spieler.java
+++ Spieler.java
@@ -1,9 +1,13 @@
 import java.util.*;
 public class Spieler {
 	private ArrayList<Beobachter> beobachter;
+	private int x;
+	private int y;
 	
 	public Spieler() {
 		beobachter=new ArrayList<Beobachter>();
+		y=50;
+		
 	}
 	
 	public void anmelden(Beobachter b) {
@@ -12,7 +16,7 @@ public class Spieler {
 	
 	public void alleInformieren() {
 		for(Beobachter b: beobachter){
-            b.ge‰ndert();
+            b.ge√§ndert();
         }
 	}
 }
