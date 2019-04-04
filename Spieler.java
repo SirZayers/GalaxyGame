db9470cb3e383a1f2bd92357438dc3788b8d0a79
Senfmeier <48903950+Senfmeier@users.noreply.github.com>
2019-04-04 08:10:49 +0200

    Update Spieler.java

--- Spieler.java
+++ Spieler.java
@@ -3,15 +3,24 @@ public class Spieler {
 	private ArrayList<Beobachter> beobachter;
 	private int x;
 	private int y;
+	private int long;
 	
 	public Spieler() {
 		beobachter=new ArrayList<Beobachter>();
 		y=50;
+		long=32;
 		
 	}
 	
 	public void anmelden(Beobachter b) {
 		beobachter.add(b);
+	}
+	public void goRight()
+	{if(x<987){x=x+5;}}
+	
+	public void goLeft()
+	{if(x<5)
+		{x=x-5;}
 	}
 	
 	public void alleInformieren() {
